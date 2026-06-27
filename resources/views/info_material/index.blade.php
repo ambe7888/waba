a42f135298093bb2275e3e0d55b5dd90521bb408
ambe7888 <ambe.akponi@uvci.edu.ci>
2026-06-27 11:51:06 +0000

    Update index.blade.php

--- resources/views/info_material/index.blade.php
+++ resources/views/info_material/index.blade.php
@@ -39,8 +39,12 @@
                                     <td>{{ Str::limit(strip_tags($material->description), 100) }}</td>
                                     <td>{{ formatDateTime($material->created_at) }}</td>
                                     <td>
-                                        <a href="{{ route('info_material.download', ['uid' => $material->_uid]) }}" class="btn btn-success btn-sm" target="_blank"><i class="fa fa-download"></i> {{ __tr('Download') }}</a>
-                                        
+                                        @if(!empty($material->__data['file_name']))
+                                            <a href="{{ route('info_material.download', ['uid' => $material->_uid]) }}" class="btn btn-success btn-sm" target="_blank"><i class="fa fa-download"></i> {{ __tr('Download') }}</a>
+                                        @endif
+                                        <button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#viewModal{{ $material->_uid }}">
+                                            <i class="fa fa-eye"></i> {{ __tr('View') }}
+                                        </button>
                                         @if(hasCentralAccess())
                                         <form action="{{ route('info_material.destroy', ['uid' => $material->_uid]) }}" method="POST" style="display:inline-block;" onsubmit="return confirm('{{ __tr('Are you sure you want to delete this material?') }}');">
                                             @csrf
@@ -66,4 +70,30 @@
         </div>
     </div>
 </div>
+
+<!-- Modals for viewing descriptions -->
+@foreach($materials as $material)
+<div class="modal fade" id="viewModal{{ $material->_uid }}" tabindex="-1" role="dialog" aria-labelledby="viewModalLabel{{ $material->_uid }}" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="viewModalLabel{{ $material->_uid }}">{{ $material->title }}</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                {!! $material->description !!}
+            </div>
+            <div class="modal-footer">
+                @if(!empty($material->__data['file_name']))
+                    <a href="{{ route('info_material.download', ['uid' => $material->_uid]) }}" class="btn btn-success" target="_blank"><i class="fa fa-download"></i> {{ __tr('Download Attached File') }}</a>
+                @endif
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">{{ __tr('Close') }}</button>
+            </div>
+        </div>
+    </div>
+</div>
+@endforeach
+
 @endsection
