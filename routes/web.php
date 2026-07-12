142a1a37867cdcd9f1afe106c432877449a8e3c6
4A Dev <dev@localhost.localdomain>
2026-07-12 21:59:24 +0000

    debug: filter debug route by contact_wa_id 22578887019

--- routes/web.php
+++ routes/web.php
@@ -1664,7 +1664,7 @@ Route::post('yoomoney/yoomoney-webhook-order-payment', [
 ])->name('yoomoney-webhook');
 
 Route::get('/debug-last-messages', function() {
-    $messages = \App\Yantrana\Components\WhatsAppService\Models\WhatsAppMessageLogModel::latest('messaged_at')->limit(5)->get();
+    $messages = \App\Yantrana\Components\WhatsAppService\Models\WhatsAppMessageLogModel::where('contact_wa_id', '22578887019')->latest('messaged_at')->limit(5)->get();
     return response()->json($messages);
 });
 
