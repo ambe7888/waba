0c795da3957c2a9c5ea85d12592ff11bc4228ac0
ambe7888 <ambe.akponi@uvci.edu.ci>
2026-07-28 20:00:19 +0000

    chore: cleanup repository, remove test files, patches, sql dumps, and extract mobile app to standalone repo

--- scratch/test_contacts_data.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$request = Illuminate\Http\Request::create('/api/vendor/contact/contacts-data', 'GET', ['assigned' => 'unassigned']);
-$response = $kernel->handle($request);
-Auth::loginUsingId(17);
-$data = app(\App\Yantrana\Components\Contact\Controllers\ContactController::class)->prepareContactList();
-print_r(array_keys($data));
