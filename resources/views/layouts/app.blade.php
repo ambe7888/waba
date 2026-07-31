109be9a52251a6a3cb72c954ce9be8beeb67735a
ambe7888 <ambe.akponi@uvci.edu.ci>
2026-07-31 21:17:54 +0000

    Fix: Restore exact width calc layout for admin dashboard to fix overlapping

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,14 +71,17 @@ $currentAppTheme ='';
     @endif
     <style>
         @media (min-width: 768px) {
-            body.lw-authenticated-page:not(.lw-minimized-menu) .main-content {
+            body:not(.lw-minimized-menu):not(.lw-guest-page) .main-content {
                 margin-left: 250px !important;
+                width: calc(100% - 250px) !important;
             }
-            body.lw-authenticated-page.lw-minimized-menu .main-content {
+            body.lw-minimized-menu:not(.lw-guest-page) .main-content {
                 margin-left: 60px !important;
+                width: calc(100% - 60px) !important;
             }
             body.lw-guest-page .main-content {
                 margin-left: 0 !important;
+                width: 100% !important;
             }
         }
         @media (max-width: 767.98px) {
